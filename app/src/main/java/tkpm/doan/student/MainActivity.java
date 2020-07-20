3cf80211b9b56c2a3f6cb7bb082b094b247bb98a
Azer <azerprojects@gmail.com>
2020-07-20 10:20:10 +0700

    + java lanuage version: 1.8

--- app/src/main/java/tkpm/doan/student/MainActivity.java
+++ app/src/main/java/tkpm/doan/student/MainActivity.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host);
     }
 }
