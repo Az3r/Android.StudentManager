773945038915b0014c907dcd1ba8cce438580dca
Azer <azerprojects@gmail.com>
2020-07-20 10:02:10 +0700

    + add layout for SplashFragment

--- app/src/main/java/tkpm/doan/student/MainActivity.java
+++ app/src/main/java/tkpm/doan/student/MainActivity.java
@@ -1,7 +1,13 @@
 package tkpm.doan.student;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
