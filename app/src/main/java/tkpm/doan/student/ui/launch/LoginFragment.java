42630ae1133f4884a71143e9dd66e4a229ddda1c
Azer <azerprojects@gmail.com>
2020-07-20 10:29:13 +0700

    fixed problem: CardView cannot load when use AppCompat theme => switch to Material theme

--- app/src/main/java/tkpm/doan/student/ui/launch/LoginFragment.java
+++ app/src/main/java/tkpm/doan/student/ui/launch/LoginFragment.java
@@ -9,11 +9,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import tkpm.doan.student.R;
+
 public class LoginFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_login, container, false);
     }
 
     @Override
--- app/src/main/java/tkpm/doan/student/ui/launch/SplashFragment.java
+++ app/src/main/java/tkpm/doan/student/ui/launch/SplashFragment.java
@@ -9,11 +9,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import tkpm.doan.student.R;
+
 public class SplashFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_splash, container, false);
     }
 
     @Override
