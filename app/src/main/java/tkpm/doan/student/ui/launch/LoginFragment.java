ccfb6879870dc97c35788674b3f1799fe2122e0e
Azer <azerprojects@gmail.com>
2020-07-20 14:21:23 +0700

    + styling login frlayout

--- app/src/main/java/tkpm/doan/student/ui/launch/LoginFragment.java
+++ app/src/main/java/tkpm/doan/student/ui/launch/LoginFragment.java
@@ -4,10 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 import tkpm.doan.student.R;
 
@@ -21,5 +26,18 @@ public class LoginFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        TextInputLayout account = view.findViewById(R.id.login_account);
+        TextInputLayout password = view.findViewById(R.id.login_password);
+        Button login = view.findViewById(R.id.button_login);
+        login.setOnClickListener(e -> onLogin(
+                account.getEditText().getText().toString(),
+                password.getEditText().getText().toString())
+        );
+    }
+
+    private void onLogin(String accounts, String password) {
+        NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host);
+        navController.navigate(R.id.action_loginFragment_to_studentFragment);
     }
 }
--- app/src/main/java/tkpm/doan/student/ui/student/StudentFragment.java
+++ app/src/main/java/tkpm/doan/student/ui/student/StudentFragment.java
@@ -2,6 +2,7 @@ package tkpm.doan.student.ui.student;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -10,6 +11,8 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentContainerView;
 import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import tkpm.doan.student.R;
 
@@ -24,8 +27,19 @@ public class StudentFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.fragment_container, new StudentProfileFragment());
-        transaction.commit();
+        BottomNavigationView bottomNavigationView = view.findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this::onMenuItemSelected);
+        bottomNavigationView.setSelectedItemId(R.id.menu_student_profile);
+
+    }
+
+    private boolean onMenuItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_student_profile) {
+            FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.fragment_container, new StudentProfileFragment());
+            transaction.commit();
+        }
+
+        return true;
     }
 }
