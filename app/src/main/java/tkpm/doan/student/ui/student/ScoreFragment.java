5657ade8aac55488adb3c58e4fa8c20c9ef5e2ab
Azer <azerprojects@gmail.com>
2020-07-31 09:29:08 +0700

    + item_notification_master.xml

--- app/src/main/java/tkpm/doan/student/ui/student/ScoreFragment.java
+++ app/src/main/java/tkpm/doan/student/ui/student/ScoreFragment.java
@@ -41,5 +41,6 @@ public class ScoreFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
     }
 }
