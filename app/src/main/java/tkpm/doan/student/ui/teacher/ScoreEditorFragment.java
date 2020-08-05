cb7303b6d23e588f5b2cd43a71e203326ec52339
azer <azerprojects@gmail.com>
2020-08-05 16:13:42 +0700

    + score editor fragment

--- app/src/main/java/tkpm/doan/student/ui/teacher/ScoreEditorFragment.java
+++ app/src/main/java/tkpm/doan/student/ui/teacher/ScoreEditorFragment.java
@@ -1,7 +1,35 @@
 package tkpm.doan.student.ui.teacher;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class ScoreEditorFragment extends Fragment {
+import tkpm.doan.student.databinding.FragmentScoreEditorBinding;
 
+public class ScoreEditorFragment extends Fragment {
+    private FragmentScoreEditorBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentScoreEditorBinding.inflate(inflater,container,false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
 }
