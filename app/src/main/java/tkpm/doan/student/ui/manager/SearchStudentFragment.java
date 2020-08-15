79eb3f5b6590a848f6a74a1d48b08a9c7dc16312
Az3r <azerprojects@gmail.com>
2020-08-15 14:32:21 +0700

    + flow from SearchStudentFragment to edit student and create notification

--- app/src/main/java/tkpm/doan/student/ui/manager/SearchStudentFragment.java
+++ app/src/main/java/tkpm/doan/student/ui/manager/SearchStudentFragment.java
@@ -1,7 +1,11 @@
 package tkpm.doan.student.ui.manager;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -9,7 +13,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDirections;
 
+import tkpm.doan.student.R;
+import tkpm.doan.student.ui.MainActivity;
 import tkpm.doan.student.databinding.FragmentSearchStudentBinding;
 
 public class SearchStudentFragment extends Fragment {
@@ -18,7 +25,7 @@ public class SearchStudentFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        binding = FragmentSearchStudentBinding.inflate(inflater,container,false);
+        binding = FragmentSearchStudentBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
 
@@ -29,7 +36,42 @@ public class SearchStudentFragment extends Fragment {
     }
 
     @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        setHasOptionsMenu(true);
+
+        MainActivity activity = (MainActivity) requireActivity();
+        activity.getBottomNav().setVisibility(View.GONE);
+    }
+
+    @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+    }
+
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.action_manager, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        MainActivity activity = (MainActivity) requireActivity();
+        NavController navController = activity.getNavController();
+        NavDirections directions = null;
+
+        switch (item.getItemId()) {
+            case R.id.nav_create_student:
+                directions = SearchStudentFragmentDirections.navigateAddStudent();
+                break;
+            case R.id.nav_create_notification:
+                directions = SearchStudentFragmentDirections.navigateCreateNotify();
+        }
+
+        if (directions != null) navController.navigate(directions);
+        return super.onOptionsItemSelected(item);
     }
 }
