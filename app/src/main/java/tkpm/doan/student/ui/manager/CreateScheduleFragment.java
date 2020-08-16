7f952fec2f350766ffab50ffda45e7819059355d
Az3r <azerprojects@gmail.com>
2020-08-16 16:15:16 +0700

    + create schedule fragment

---
+++ app/src/main/java/tkpm/doan/student/ui/manager/CreateScheduleFragment.java
@@ -0,0 +1,35 @@
+package tkpm.doan.student.ui.manager;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import tkpm.doan.student.databinding.FragmentCreateScheduleBinding;
+
+public class CreateScheduleFragment extends Fragment {
+
+    private FragmentCreateScheduleBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentCreateScheduleBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
